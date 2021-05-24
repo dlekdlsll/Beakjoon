8f3ff9171f37c6101129cb8986abd7f39bd9a50c
dlekdlsll <dtp05093@naver.com>
2021-05-24 21:21:46 +0900

    백준 알고리즘 5622번_도전중

---
+++ Baekjoon_5622.py
@@ -0,0 +1,11 @@
+inin = input()
+inin = list(inin)
+A, B, C = 2, 2, 2
+D, E, F = 3, 3, 3
+G, H, I = 4, 4, 4
+J, K, L = 5, 5, 5
+M, N, O, = 6, 6, 6
+P, Q, R, S = 7, 7, 7, 7
+T, U, V = 8, 8, 8
+W, X, Y, Z = 9, 9, 9, 9
+[print(i) for i in inin]
