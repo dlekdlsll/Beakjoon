5c141d4774fffeca61e612e4f3e0d0b76d404626
dlekdlsll <dtp05093@naver.com>
2021-05-25 23:41:10 +0900

    백준 알고리즘 5622번

--- Baekjoon_5622.py
+++ Baekjoon_5622.py
@@ -1,11 +1,29 @@
 inin = input()
 inin = list(inin)
-A, B, C = 2, 2, 2
-D, E, F = 3, 3, 3
-G, H, I = 4, 4, 4
-J, K, L = 5, 5, 5
-M, N, O, = 6, 6, 6
-P, Q, R, S = 7, 7, 7, 7
-T, U, V = 8, 8, 8
-W, X, Y, Z = 9, 9, 9, 9
-[print(i) for i in inin]
+answer = []
+result = 0
+
+for i in inin:
+    if i in('A','B','C'):
+        answer.append(3)
+    elif i in ('D','E','F'):
+        answer.append(4)
+    elif i in ('G','H','I'):
+        answer.append(5)
+    elif i in ('J','K','L'):
+        answer.append(6)
+    elif i in ('M','N','O'):
+        answer.append(7)
+    elif i in ('P','Q','R','S'):
+        answer.append(8)
+    elif i in ('T','U','V'):
+        answer.append(9)
+    elif i in ('W','X','Y','Z'):
+        answer.append(10)
+    elif i == '1':
+        answer.append(2)
+    elif i == '0':
+        answer.append(11)
+for i in answer:
+    result += i
+print(result)
