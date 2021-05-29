c7daff1eaafa5ee191eae1a1c9eb75b467d290d9
dlekdlsll <dtp05093@naver.com>
2021-05-29 17:08:42 +0900

    백준 알고리즘 2869번 수정(미완)

--- Baekjoon_2869.py
+++ Baekjoon_2869.py
@@ -2,11 +2,7 @@ import sys
 import math
 
 A, B, V = map(int,sys.stdin.readline().split())
-# answer = V/(A - B)
-# print(math.ceil(answer))
-
-# answer = ((V/A) + (V/B)) / A  
-# print(math.ceil(answer))
-
-# answer = ((V/(A-B))+A) / A  
-# print(math.ceil(answer))
+ 
+answer = (V/(A-B) - A) + 1
+print(answer)
+print(abs(math.ceil(answer)))
