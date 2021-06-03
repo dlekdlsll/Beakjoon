7a7f71fa375264aefbfdb314c07126d09dd6c65a
dlekdlsll <dtp05093@naver.com>
2021-06-03 12:52:34 +0900

    백준 알고리즘 2775번

---
+++ Baekjoon_2775.py
@@ -0,0 +1,15 @@
+from typing import AnyStr
+
+
+T = int(input())
+
+answer = []
+solve = 0
+for i in range(0,T):
+    k = int(input())
+    n = int(input())
+    for j in range(1,n+1):
+        solve += j
+    answer.append(solve)
+    solve = 0
+[print(a) for a in answer]
