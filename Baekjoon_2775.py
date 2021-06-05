65b1f47e2e04607474715e5b854d1aece837ed76
dlekdlsll <dtp05093@naver.com>
2021-06-05 13:15:19 +0900

    백준 알고리즘 2775번 수정중

--- Baekjoon_2775.py
+++ Baekjoon_2775.py
@@ -1,13 +1,19 @@
 T = int(input())
 floor0 = [i for i in range(1, 15)]
-
+times = 0
 answer = []
 solve = 0
+TC = []
 for i in range(0,T):
     k = int(input())
     n = int(input())
     for j in range(1,n+1):
+        test = 1 + times
         solve += j
+        times += j
     answer.append(solve)
+    TC.append(test)
     solve = 0
+
 [print(a) for a in answer] 
+[print(a) for a in TC] 
