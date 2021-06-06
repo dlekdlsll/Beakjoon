1e1b991c3be7b3c84de57fc8f1233dac711f1fd8
dlekdlsll <dtp05093@naver.com>
2021-06-06 14:40:11 +0900

    백준 알고리즘 2869번 수정(최종)

--- Baekjoon_2869.py
+++ Baekjoon_2869.py
@@ -2,10 +2,6 @@ import sys
 import math
 
 A, B, V = map(int,sys.stdin.readline().split())
-
-if A == V:
-    answer = 1
-    print(answer)
-else:
-    answer = (V/(A-B) - A) + 1
-    print(abs(math.ceil(answer)))
+V = V-A
+answer = (V/(A-B)) + 1
+print(math.ceil(answer))
