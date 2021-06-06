b2a56664687c4c5e8294c2a0b303a1fe6875e04a
dlekdlsll <dtp05093@naver.com>
2021-06-06 13:11:58 +0900

    백준 알고리즘 2839 수정중

--- Baekjoon_2839.py
+++ Baekjoon_2839.py
@@ -1,10 +1,14 @@
 N = int(input())
 answer = 0
 
-if N % 5 > 2 and N//5 > 1 :
+if N % 5 > 1 and N//5 > 1 :
     namergy = N%5
     if namergy%3==0:
         answer = (N//5 + namergy//3)
+elif N % 3 > 1 and N//3 > 1 :
+    namergy = N%3
+    if namergy%5==0:
+        answer = (N//3 + namergy//5)
 elif N % 3 == 0:
     answer = N//3
 elif N % 3 == 2:
