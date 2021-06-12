af2bd606b97b73e987a6826a4b05ddf0edd4a1a4
dlekdlsll <dtp05093@naver.com>
2021-06-12 17:24:01 +0900

    백준 알고리즘 1011번(수정중3)

--- Baekjoon_1011.py
+++ Baekjoon_1011.py
@@ -1,23 +1,19 @@
+import math
 T = int(input())
 case = []
 
 for i in range(0,T):   
     x, y = map(int,input().split())
-    d = y-x-1
-    distance = [-1,0,1]
+    d = round((y-x)/2)
     times = 1
-    jump = 1
+    N = 1
 
     while True:
-        if d == 0:
+        if N > d:
             break
-        d -= jump
+        N += N
         times += 1
-        distance = [jump-1,jump,jump+1]
-        if d in distance and max(distance) <= 3 and d > 0:
-            d -= d
-            times += 1
-        jump = max(distance)
-    case.append(times)
-    
+    jump = (times*2)+1
+    case.append(jump)
+
 [print(i) for i in case]
