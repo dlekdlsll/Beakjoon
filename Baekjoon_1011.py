2727dd1b2aad0954bcec6fd48fb3974b51c4a628
dlekdlsll <dtp05093@naver.com>
2021-06-13 14:12:50 +0900

    백준 알고리즘 1011번 (수정중 3)

--- Baekjoon_1011.py
+++ Baekjoon_1011.py
@@ -4,16 +4,16 @@ case = []
 
 for i in range(0,T):   
     x, y = map(int,input().split())
-    d = round((y-x)/2)
+    d = round(((y-x)/2)-1,)
     times = 1
     N = 1
 
     while True:
-        if N > d:
-            break
         N += N
         times += 1
-    jump = (times*2)+1
+        if N > d:
+            break
+    jump = times+1
     case.append(jump)
 
 [print(i) for i in case]
