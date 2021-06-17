9340fd33df0bedf63564c96acb37e9c9d06662c5
dlekdlsll <dtp05093@naver.com>
2021-06-17 21:20:53 +0900

    백준 알고리즘 11653번

---
+++ Baekjoon_11653.py
@@ -0,0 +1,17 @@
+N = int(input())
+
+i =  2
+soinsu = []
+
+while True:
+  if N == 1:
+    soinsu.sort
+    [print(j) for j in soinsu]
+    break
+
+  if N % i ==0:
+    soinsu.append(i)
+    N = N//i
+
+  elif N % i!=0:
+    i += 1
