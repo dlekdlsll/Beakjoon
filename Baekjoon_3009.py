9816b45474a7d97955d52e35ad890244a639e91b
dlekdlsll <dtp05093@naver.com>
2021-06-29 13:03:24 +0900

    정말 마지막 테스트

--- Baekjoon_3009.py
+++ Baekjoon_3009.py
@@ -2,7 +2,7 @@ X, Y = [],[]
 for _ in range(3):    # 인자없이 3번 반복합니다
     x, y = map(int, input().split())
     X.append(x); Y.append(y)
-for i in range(3):   # 3번 반복
+for i in range(3):   # 3번 반복 합니다
     if X.count(X[i]) == 1: x2 = X[i]
     if Y.count(Y[i]) == 1: y2 = Y[i]
 print(x2, y2)
