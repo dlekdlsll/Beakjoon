8512d137766359cb70fa100bd434ac289712c592
dlekdlsll <dtp05093@naver.com>
2021-07-03 20:16:21 +0900

    백준 알고리즘 10872번(수정중2)

--- Baekjoon_10872.py
+++ Baekjoon_10872.py
@@ -1,4 +1,7 @@
 N = int(input())
-for i in range(1,N):
-    N = N*i
-print(N)
+if N == 1: print(1)
+if N == 0: print(0)
+if N != 1 and N != 0:
+    for i in range(N-1,1,-1):
+        N = N*i
+    print(N)
